6715a1833ad0a3df6f958e47ca4d2c24f51cc2e0
Ben L <39037350+blawson69@users.noreply.github.com>
2019-08-22 16:54:57 -0700

    Added current value searching

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # RepPrinter
 
-Originally created to output a [5e Shaped Sheet](http://github.com/mlenser/roll20-character-sheets/tree/master/5eShaped) character's repeating item attributes into the API Output Console for reference when developing other scripts. Now the script allows passing a random string (no spaces) to print any character attribute that matches that string.
+Originally created to output a [5e Shaped Sheet](http://github.com/mlenser/roll20-character-sheets/tree/master/5eShaped) character's repeating item attributes into the API Output Console for reference when developing other scripts. The script allows passing a random string (no spaces) to print any character attribute that matches that string, or searching for a string in the "current" value of an attribute.
 
 ## How to Use
 
-Sent the `!print` command along with the ID of the character you are referencing then the category of repeating items you wish to view. Valid categories are "equipment", "spells", "utility", "offense", "ammo", "skills", "armor", "class", "modifiers", and "attachers". You may also narrow the spells down to the spell level by using "spell" plus the level with no spaces, using "spell0" for cantrips.
+Sent the `!print` command along with the ID of the character you are referencing then the category of repeating items you wish to view. Valid categories are "equipment", "spells", "utility", "offense", "ammo", "skills", "armor", "class", "modifiers", "classfeature" and "attachers". You may also narrow the spells down to the spell level by using "spell" plus the level with no spaces, using "spell0" for cantrips.
 
 You can send "random" as the category to search for a random string, followed by the string you wish to search. This string does not have to be a repeating item but must contain no spaces.
 
+You can use "curval" as the category to search for a string value in an attribute's "current" field. This can be any string with spaces and punctuation.
+
 Here are some sample macros:
 
-`!print @{selected|character_id} ?{Category|Equipment|Spells|Utility|Offense|Ammo|Skills|Armor|Class|Modifiers|Attachers|Random,random ?&#123;String&#125;}`
+`!print @{selected|character_id} ?{Category|Equipment|Spells|Utility|Offense|Ammo|Skills|Armor|Class|Modifiers|Class Features,classfeature|Attachers|Random,random ?&#123;String&#125;|Value,curval ?&#123;Value&#125;}`
 
 `!print @{selected|character_id} ?{Spell Level|All,spells|Cantrips,spell0|Level 1,spell1|Level 2,spell2|Level 3,spell3|Level 4,spell4|Level 5,spell5|Level 6,spell6|Level 7,spell7|Level 8,spell8|Level 9,spell9}`
--- RepPrinter.js
+++ RepPrinter.js
@@ -12,8 +12,8 @@ var RepPrinter = RepPrinter || (function () {
 
     //---- INFO ----//
 
-    var version = '0.1',
-		repstrings = {equipment: '^(?:repeating_equipment_).+$', utility: '^(?:repeating_utility_).+$', offense: '^(?:repeating_offense_).+$', ammo: '^(?:repeating_ammo_).+$', spells: '^(?:repeating_spell).+$', spell0: '^(?:repeating_spell0).+$', spell1: '^(?:repeating_spell1).+$', spell2: '^(?:repeating_spell2).+$', spell3: '^(?:repeating_spell3).+$', spell4: '^(?:repeating_spell4).+$', spell5: '^(?:repeating_spell5).+$', spell6: '^(?:repeating_spell6).+$', spell7: '^(?:repeating_spell7).+$', spell8: '^(?:repeating_spell8).+$', spell9: '^(?:repeating_spell9).+$', skills: '^(?:repeating_skill_).+$', armor: '^(?:repeating_armor_).+$', class: '^(?:repeating_class_).+$', modifiers: '^(?:repeating_modifier_).+$', attachers: '^(?:repeating_attacher_).+$', classFeature: '^(?:repeating_classfeature_).+$', random: '^.*REPLACE.*$'},
+    var version = '0.2',
+		repstrings = {equipment: '^(?:repeating_equipment_).+$', utility: '^(?:repeating_utility_).+$', offense: '^(?:repeating_offense_).+$', ammo: '^(?:repeating_ammo_).+$', spells: '^(?:repeating_spell).+$', spell0: '^(?:repeating_spell0).+$', spell1: '^(?:repeating_spell1).+$', spell2: '^(?:repeating_spell2).+$', spell3: '^(?:repeating_spell3).+$', spell4: '^(?:repeating_spell4).+$', spell5: '^(?:repeating_spell5).+$', spell6: '^(?:repeating_spell6).+$', spell7: '^(?:repeating_spell7).+$', spell8: '^(?:repeating_spell8).+$', spell9: '^(?:repeating_spell9).+$', skills: '^(?:repeating_skill_).+$', armor: '^(?:repeating_armor_).+$', class: '^(?:repeating_class_).+$', modifiers: '^(?:repeating_modifier_).+$', attachers: '^(?:repeating_attacher_).+$', classFeature: '^(?:repeating_classfeature_).+$', random: '^.*REPLACE.*$', curval: '^.*REPLACE.*$'},
         styles = {
             box:  'background-color: #fff; border: 1px solid #000; padding: 8px 10px; border-radius: 6px; margin-left: -40px; margin-right: 0px;',
             title: 'padding: 0 0 10px 0; color: ##591209; font-size: 1.5em; font-weight: bold; font-variant: small-caps; font-family: "Times New Roman",Times,serif;',
@@ -34,49 +34,82 @@ var RepPrinter = RepPrinter || (function () {
 			var parms = msg.content.split(/\s+/i);
 			if (parms.length > 2) {
 				var char_id = parms[1];
-				if (parms[2].match(/^(?:equipment|spell[s0-9]{1}|utility|offense|ammo|skills|armor|class|modifiers|attachers|classfeature|random)$/i) == null) {
+				if (parms[2].match(/^(?:equipment|spell[s0-9]{1}|utility|offense|ammo|skills|armor|class|modifiers|attachers|classfeature|random|curval)$/i) == null) {
 					var valid_categories = _.map(_.keys(repstrings), function(x) {return x;});
-                    showDialog('Error', '"' + parms[2] + '" is not a valid category. Valid categories are equipment, spells, utility, offense, ammo, skills, armor, class, modifiers, classfeature and attachers.<br><br>You may also narrow the spells down by using "spell" plus the spell level with no spaces. Use spell0 for cantrips.<br><br>To search for other strings, use "random" as the category and send your string as the last option.<br><br>Please try again.');
+                    showDialog('Error', '"' + parms[2] + '" is not a valid category. Valid categories are equipment, spells, utility, offense, ammo, skills, armor, class, modifiers, classfeature, and attachers.<br><br>You may also narrow the spells down by using "spell" plus the spell level with no spaces. Use spell0 for cantrips.<br><br>To search for other strings, use "random" as the category and send your string as the last option. To search the current value of an attribute, use "curval" as the category followed by your string.<br><br>Please try again.');
 				} else {
-					var category = parms[2].toLowerCase();
+					var category = parms[2].toLowerCase(), search_string = parms[2].toLowerCase();
                     var character = getObj('character', char_id);
                     var char_name = (character) ? character.get('name') : char_id;
 					var charAttrs = findObjs({type: 'attribute', characterid: char_id}, {caseInsensitive: true});
 					if (charAttrs) {
-                        var tmpCat = repstrings[category];
-                        if (category == 'random') {
-                            tmpCat = tmpCat.replace('REPLACE', esRE(parms[3]));
-                            category = parms[3];
+                        if (category == 'curval') {
+                            var tmpCat = repstrings[category];
+                            search_string = msg.content.substring(35);
+                            tmpCat = tmpCat.replace('REPLACE', esRE(search_string));
+    						var re = new RegExp(tmpCat, 'i');
+                            var util = _.filter(charAttrs, function (attr) {
+                                let x = attr.get('current') + '';
+                                return (x.match(re) !== null);
+                            });
+    						if (util && util.length > 0) {
+                                showDialog('', util.length + ' "' + search_string + '" search results found for ' + char_name + '. Printing...');
+    							log('.'); // spacer
+    							log('-------------- BEGIN "' + search_string + '" search results for ' + char_name + ' --------------');
+    							var row_id = '';
+    							_.each(util, function (row) {
+    								var tmp_row_id = row.get('name').split('_')[2];
+    								if (row_id !== '' && row_id !== tmp_row_id) {
+    									log('--------------');
+    								}
+    								log(row.get('name') + ' = ' + row.get('current'));
+    								row_id = tmp_row_id;
+    							});
+                                log('--------------- END "' + search_string + '" search results for ' + char_name + ' ---------------');
+                                setTimeout(function () {
+                                    showDialog('Complete', 'Printed ' + util.length + ' "' + search_string + '" attribute searches for ' + char_name + '.');
+                                }, 1000);
+    						} else {
+                                showDialog('Not Found', char_name + ' has no "' + search_string + '" attributes.');
+                            }
+
+                        } else {
+                            var tmpCat = repstrings[category];
+                            if (category == 'random') {
+                                search_string = msg.content.substring(35);
+                                tmpCat = tmpCat.replace('REPLACE', esRE(search_string));
+                            }
+                            var re = new RegExp(tmpCat, 'i');
+                            var util = _.filter(charAttrs, function (attr) {
+                                return (attr.get('name').match(re) !== null);
+                            });
+                            if (util && util.length > 0) {
+                                showDialog('', util.length + ' "' + search_string + '" attributes found for ' + char_name + '. Printing...');
+                                log('.'); // spacer
+                                log('-------------- BEGIN ' + search_string + ' attributes for ' + char_name + ' --------------');
+                                var row_id = '';
+                                _.each(util, function (row) {
+                                    var tmp_row_id = row.get('name').split('_')[2];
+                                    if (row_id !== '' && row_id !== tmp_row_id) {
+                                        log('--------------');
+                                    }
+                                    log(row.get('name') + ' = ' + row.get('current'));
+                                    row_id = tmp_row_id;
+                                });
+                                log('--------------- END ' + search_string + ' attributes for ' + char_name + ' ---------------');
+                                setTimeout(function () {
+                                    showDialog('Complete', 'Printed ' + util.length + ' "' + search_string + '" attributes for ' + char_name + '.');
+                                }, 1000);
+                            } else {
+                                showDialog('Not Found', char_name + ' has no "' + search_string + '" attributes.');
+                            }
+
                         }
-						var re = new RegExp(tmpCat, 'i');
-						var util = _.filter(charAttrs, function (attr) {
-							return (attr.get('name').match(re) !== null);
-						});
-						if (util && util.length > 0) {
-                            showDialog('', util.length + ' "' + category + '" attributes found for ' + char_name + '. Printing...');
-							log('.'); // spacer
-							log('-------------- BEGIN ' + category + ' attributes for ' + char_name + ' --------------');
-							var row_id = '';
-							_.each(util, function (row) {
-								var tmp_row_id = row.get('name').split('_')[2];
-								if (row_id !== '' && row_id !== tmp_row_id) {
-									log('--------------');
-								}
-								log(row.get('name') + ' = ' + row.get('current'));
-								row_id = tmp_row_id;
-							});
-                            log('--------------- END ' + category + ' attributes for ' + char_name + ' ---------------');
-                            setTimeout(function () {
-                                showDialog('Complete', 'Printed ' + util.length + ' "' + category + '" attributes for ' + char_name + '.');
-                            }, 1000);
-						} else {
-                            log('tmpCat = ' + tmpCat);
-                            showDialog('Not Found', char_name + ' has no "' + category + '" attributes.');
-                        }
+
 					}
 				}
 			} else {
-                showDialog('Error', 'You didn\'t send all the required parameters. Proper syntax is:<br><br><span style=\'' + styles.code + '\'>!print character_id category</span><br><br>Try again.');
+                showDialog('Error', 'You didn\'t send all the required parameters. Proper syntax is:<br><br><span style=\'' + styles.code + '\'>!print character_id category [search_criteria]</span><br><br>Try again.');
 			}
 		}
     },
