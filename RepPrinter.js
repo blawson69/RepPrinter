91fe972799910994154e8431ba986f9ef00c412c
Ben L <39037350+blawson69@users.noreply.github.com>
2019-08-30 19:24:16 -0700

    Update RepPrinter.js
    Corrected Venmo link

--- RepPrinter.js
+++ RepPrinter.js
@@ -4,7 +4,7 @@ Outputs a 5e Shaped Sheet character's repeating items into the API Output Consol
 
 On Github:	https://github.com/blawson69
 Contact me: https://app.roll20.net/users/1781274/ben-l
-Like this script? Buy me a coffee: https://venmo.com/theBenLawson
+Like this script? Buy me a coffee: https://venmo.com/theRealBenLawson
 */
 
 var RepPrinter = RepPrinter || (function () {
